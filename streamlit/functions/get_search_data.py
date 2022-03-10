25180f88b4ed8ca0a574a4cfbe84837314d9dd26
Oliver Giles <ogiles@email.com>
2022-03-10 16:26:33 +0000

    added tagging

--- streamlit/functions/get_search_data.py
+++ streamlit/functions/get_search_data.py
@@ -1,5 +1,50 @@
 import json
 import gcsfs
+from random import shuffle
+from top2vec import Top2Vec
+import os
+import re
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(base_path, "model", "model_deep_L")
+model_ttl = Top2Vec.load(model_path)
+
+def cleaner(text, **kwargs):
+  """params is a list of things to remove: codec, acronyms, numbers, brackets, page, para, legal formatting """
+  if not 'params' in kwargs:
+    kwargs['params'] = ['codec', 'acronyms', 'numbers', 'brackets', 'page', 'para', 'legal formatting', 'info']
+  if 'codec' in kwargs['params']:
+    text_encoded = text.encode('ascii', errors = 'ignore')
+    text_decode = text_encoded.decode()
+    clean_text = " ".join([word for word in text_decode.split()])
+    text = clean_text
+  if 'numbers' in kwargs['params']:
+    pattern = r'[0-9]'
+    text = re.sub(pattern, '', text)
+  if 'brackets' in kwargs['params']:
+    text = re.sub('\(.*?\)', '', text)
+    text = re.sub('\[.*?\]', '', text)
+  if 'acronyms' in kwargs['params']:
+    text = text.split()
+    clean_text = []
+    for word in text:
+      if any(l.islower() for l in word):
+        clean_text.append(word)
+    text = ' '.join(clean_text)
+  if 'page' in kwargs['params']:
+      text = re.sub('Page [0-9]+', '', text)
+  if 'para' in kwargs['params']:
+      text = re.sub('Para [0-9]+', '', text)
+  if 'legal formatting' in kwargs['params']:
+      text = re.sub('Civ', '', text)
+      text = re.sub('On appeal from:', '', text)
+  if 'info' in kwargs['params']:
+      text = re.sub("NOTE.*", "", text)
+  return text
+
+def get_tag(lst_str):
+    res = model_ttl.query_topics(query = lst_str, num_topics=1)
+    return list(res[0][0][:3])
 
 def open_from_bucket():
     gcs_file_system = gcsfs.GCSFileSystem()
@@ -15,11 +60,12 @@ def create_search_data(data):
     search_data = {}
     for i, case in enumerate(data):
         KEY = i
+        tag_data = case['press summary']['Background to the appeal'][:100]
         data = {
             'name':case['details']['Name'],
             'date':case['details']['Judgment date'],
             'citation':case['details']['Neutral citation'],
-            'tags': ["law", "judge", "case"],
+            'tags': get_tag(tag_data),
             'content':[case['press summary']['Background to the appeal'][:150],
                        case['press summary']['Reasons for the judgment'][:150],
                        case['details']['Neutral citation'],
